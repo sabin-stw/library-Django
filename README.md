e4286342200e9a14cfaa19d0fd8f09965176ba99
krishna72000 <sksharma72000@gmail.com>
2022-03-05 22:56:50 +0545

    student detail and design added

---
+++ README.md
@@ -0,0 +1,9 @@
+Library Management System
+
+Step 1: Install python3.8
+Step 2: Install pip3
+Step 3: python -m pip install Django
+Step 4: create adminuser using following command inside project
+        python manage.py createsuperuser
+
+Step 5: python manage.py runserver
---
+++ library/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime,timedelta
+
+
+class Book(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    isbn = models.PositiveIntegerField()
+    category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.name) + " ["+str(self.isbn)+']'
+
+class Student(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    classroom = models.CharField(max_length=10)
+    branch = models.CharField(max_length=10)
+    roll_no = models.CharField(max_length=3, blank=True)
+    phone = models.CharField(max_length=10, blank=True)
+    # image = models.ImageField(upload_to="", blank=True)
+
+    def __str__(self):
+        return str(self.user) + " ["+str(self.branch)+']' + " ["+str(self.classroom)+']' + " ["+str(self.roll_no)+']'
+
+
+def expiry():
+    return datetime.today() + timedelta(days=14)
+class IssuedBook(models.Model):
+    id = models.AutoField(primary_key=True)
+    student_id = models.CharField(max_length=100, blank=True) 
+    isbn = models.CharField(max_length=13)
+    issued_date = models.DateField(auto_now=True)
+    expiry_date = models.DateField(default=expiry)
